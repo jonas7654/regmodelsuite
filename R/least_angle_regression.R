9b72097e30cf246b3a519205f1415132910d466f
jonas7654 <veit.jonas@googlemail.com>
2024-07-28 15:57:03 +0200

    least angle basic implementation works now

--- R/least_angle_regression.R
+++ R/least_angle_regression.R
@@ -1,51 +1,42 @@
 
-least_angle_regression <- function(X, y, iter = 1000) {
-
-  # standardize regressors
-  X_scaled <- scale(X)
+least_angle_regression <- function(X, y, iter = 10) {
 
   n <- nrow(X)
   p <- ncol(X)
+  coefficient_matrix <- matrix(NA, iter, p)
 
-  # Initialize values
-  beta <- rep(0, p)
-  r <- y - mean(y)  # residua
-  active_set <- logical(p)
-  x_candidates <- 1:p
+  # Standardize regressors and initialize the first residuum
+  X_scaled <- scale(X)
+  r <- y - mean(y)
+
+  # Helper
+  active_variables <- logical(p)
+  beta <- double(p)
 
   for (i in 1:iter) {
+    # Find the regressor x most correlated with current r
+    r_cor <- cor(X_scaled[, !active_variables], r)
+    x_index <- which.max(abs(r_cor))
 
-    # calculate the correlation between r and x_j
-    r_x_cor <- cor(r, X_scaled)
+    # update the active set
+    active_variables[x_index] <- TRUE
+    A <- X_scaled[, active_variables]
 
-    # Get index of x_i which has the highest correlation
-    x_index <- which.max(abs(r_x_cor))
+    # Calculate the current model and update beta
+    delta_step <- solve(t(A) %*% A, t(A) %*% r)
+    beta[active_variables] <- beta[active_variables] + delta_step
 
-    # Update the active set
-    active_set[x_index] <- TRUE
-    x_candidates <- setdiff(x_candidates, x_index)
+    # update the residual
+    r <- y - A %*% as.matrix(beta[active_variables])
 
-
-    A <- X[, active_set]
-
-    # Calculate delta and u
-    delta = solve(t(A) %*% A, t(A) %*% r)
-    u <- A %*% delta
-
-    # Move betas
-    beta[x_index] <- beta[x_index] + alpha * delta
-
-
-    # TODO
-
-
-    # TODO
-
-
-    # update residual
-    r <- y - X_scaled %*% beta
+    # update output data
+    coefficient_matrix[i, ] <- beta
+    print(as.integer(active_variables))
   }
 
-  return (list(delta, beta, u , r))
+
+
+
+  return (list(coefficient_matrix))
 
 }
