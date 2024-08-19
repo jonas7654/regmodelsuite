fda6449c439c05100735958e7d4a70fe59d36297
jonas7654 <veit.jonas@googlemail.com>
2024-08-19 19:09:34 +0200

    lar test

--- tests/testthat/test-LAR_test.R
+++ tests/testthat/test-LAR_test.R
@@ -1,3 +1,5 @@
+set.seed(2421)
+
 n <- 25
 p <- 50
 
@@ -27,16 +29,16 @@ test_that("LAR works", {
 
   expect_equal(LAR_fit$mean_y, mean(y))
   expect_equal(LAR_fit$mean_x, colMeans(X))
-  expect_equal(LAR_fit$sd_x, apply(X,2 ,function(x) {sd(x)}))
+  expect_equal(LAR_fit$sd_x, apply(X, 2 ,function(x) {sd(x)}))
 
   # test coef
   coef_test_LAR <- coef(LAR_fit)
 
-  expect_equal(coef_test_LAR, LAR_fit$coefficients)
+  expect_equal(coef_test_LAR, LAR_fit$coefficients[n - 1, ])
 
   # Test predict
   # back to original scale
-  manual_predict <- as.vector(mean(y) + as.matrix(scale(X)) %*% coef_test_LAR)
+  manual_predict <- mean(y) + scale(X) %*% coef_test_LAR
 
   expect_equal(manual_predict, predict(LAR_fit))
 
