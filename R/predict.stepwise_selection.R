97a7d53954833f1b1b8fce9a2527a9c06f2fa230
jonas7654 <veit.jonas@googlemail.com>
2024-08-21 10:01:54 +0200

    implemented predict for forward selection

--- R/predict.stepwise_selection.R
+++ R/predict.stepwise_selection.R
@@ -1,8 +1,12 @@
 #' Predict function for stepwise_selection()
 
-predict.stepwise_selection <- function(object, newdata = NULL) {
+predict.stepwise_selection <- function(selectionobject, newdata = NULL) {
   stopifnot("object must be of type stepwise_selection" =
-              inherits(object,stepwise_selection))
+              inherits(selectionobject,"stepwise_selection"))
 
+  object <- selectionobject$model
 
+  y_hat <- predict(object , newdata)
+
+  return(y_hat)
 }
